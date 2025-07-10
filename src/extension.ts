d14d0e46e4887ce6653258086226c9c349a95fbc
Eli Barzilay <eli@barzilay.org>
2025-07-10 11:12:33 -0400

    Improve shell command and much more

--- src/extension.ts
+++ src/extension.ts
@@ -3,83 +3,119 @@ import { dirname, join } from "path";
 import { writeFileSync } from "fs";
 import { env } from "process";
 
+const cmd = (name: string, args?: any) =>
+  vscode.commands.executeCommand(name, args);
+
+const say = (str: string) =>
+  vscode.window.showInformationMessage(str);
+
 const updateConfig = async () => {
   const conf = vscode.workspace.getConfiguration();
   const name = "terminal.integrated.commandsToSkipShell";
   const val = conf.get(name);
   if (!Array.isArray(val)) return;
   if (val.includes("elicode.shellEnter")) return;
-  await conf.update(name, [...val, "elicode.shellEnter"], vscode.ConfigurationTarget.Global);
+  await conf.update(
+    name, [...val, "elicode.shellEnter"], vscode.ConfigurationTarget.Global);
   console.log(`Updated ${name} setting`);
 };
 
+const cursorMove = async (
+  editor: vscode.TextEditor, _edit: vscode.TextEditorEdit, opts: any
+) => {
+  const sels = editor.selections.filter(s => !s.isEmpty);
+  if (sels.length && !opts?.select) {
+    // await cmd("cancelSelection");
+    editor.selections = sels.map(s => new vscode.Selection(s.active, s.active));
+  }
+  return cmd("cursorMove", opts);
+};
+
+// Doesn't work because I can't figure out how to change the config options
+const _reindentSelectionWithSpaces = async () => {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) return;
+  const qp = vscode.window.createQuickPick();
+  qp.items = "12345678".split("").map(n => ({ label: n }));
+  qp.placeholder = "Select indentation level";
+  qp.activeItems = [qp.items[1]]; // Default to "2"
+  const spaces = await new Promise<number>((res) => {
+    const ret = (n: number) => { qp.dispose(); res(n); };
+    qp.onDidAccept(() => ret(parseInt(qp.selectedItems[0]?.label || "0")));
+    qp.onDidHide(() => ret(-1));
+    qp.show();
+  });
+  if (spaces === -1) return;
+  Object.assign(editor.options, { tabSize: spaces, insertSpaces: true });
+  await cmd("editor.action.reindentselectedlines");
+};
+
+type ShellArgs = { sync?: boolean, new?: boolean, inTerm?: boolean };
+let syncShellTimeout: NodeJS.Timeout | null = null;
+const openShell = async (args: ShellArgs) => {
+  const home = env.HOME;
+  if (!home) return;
+  const uri = vscode.window.activeTextEditor?.document.uri;
+  const file = uri ? uri.path : "";
+  const wdir =
+    uri && vscode.workspace.getWorkspaceFolder(uri)?.uri.path
+    || vscode.workspace.workspaceFolders?.[0]?.uri.path
+    || "";
+  let term = vscode.window.activeTerminal;
+  // Shell context info
+  const shQuote = (str: string) => str.replace(/'/g, "'\\''");
+  writeFileSync(join(home, ".vscode-sync"), [
+    `file='${shQuote(file)}'`,
+    `wdir='${shQuote(wdir ?? "")}'`,
+    `chdir='yes'`,
+  ].join("\n") + "\n");
+  if (args?.inTerm) {
+    if (!syncShellTimeout) {
+      return await cmd("workbench.action.terminal.toggleTerminal");
+    } else {
+      clearTimeout(syncShellTimeout);
+      syncShellTimeout = null;
+    }
+  } else if (!term || args?.new) {
+    term = vscode.window.createTerminal({ cwd: dirname(file) });
+  } else if (args?.sync && term) {
+    // Notify shell to sync info after a cancelable delay
+    if (syncShellTimeout) clearTimeout(syncShellTimeout);
+    syncShellTimeout = setTimeout(async () => {
+      syncShellTimeout = null;
+      const pid = await term?.processId;
+      if (pid) process.kill(pid, "SIGUSR1");
+    }, 500);
+  }
+  return term?.show(false);
+};
+
+type DelayedCommandArgs = { delay: number, command: string, args: object };
+const delayedCommand = (args: DelayedCommandArgs) =>
+  new Promise<void>((res) => {
+    setTimeout(() => {
+      cmd(args.command, args.args ?? {});
+      res();
+    }, args.delay);
+  });
+
+const commands = [
+  openShell,
+  delayedCommand,
+  // reindentSelectionWithSpaces,
+];
+const editorCommands = [
+  cursorMove,
+];
+
 export const activate = async (context: vscode.ExtensionContext) => {
   await updateConfig();
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand(
-      "elicode.cursorMove",
-      async (editor, _edit, opts) => {
-        const sels = editor.selections.filter(s => !s.isEmpty);
-        if (sels.length && !opts?.select) {
-          // await vscode.commands.executeCommand("cancelSelection");
-          editor.selections = sels.map(s => new vscode.Selection(s.active, s.active));
-        }
-        return vscode.commands.executeCommand("cursorMove", opts);
-      }),
-    // Doesn't work because I can't figure out how to change the config options
-    // vscode.commands.registerCommand(
-    //   "elicode.reindentSelectionWithSpaces",
-    //   async () => {
-    //     const editor = vscode.window.activeTextEditor;
-    //     if (!editor) return;
-    //     const qp = vscode.window.createQuickPick();
-    //     qp.items = "12345678".split("").map(n => ({ label: n }));
-    //     qp.placeholder = "Select indentation level";
-    //     qp.activeItems = [qp.items[1]]; // Default to "2"
-    //     const spaces = await new Promise<number>((res) => {
-    //       const ret = (n: number) => { qp.dispose(); res(n); };
-    //       qp.onDidAccept(() => ret(parseInt(qp.selectedItems[0]?.label || "0")));
-    //       qp.onDidHide(() => ret(-1));
-    //       qp.show();
-    //     });
-    //     if (spaces === -1) return;
-    //     Object.assign(editor.options, { tabSize: spaces, insertSpaces: true });
-    //     await vscode.commands.executeCommand("editor.action.reindentselectedlines");
-    //   }),
-    vscode.commands.registerCommand(
-      "elicode.openShell",
-      () => {
-        const home = env.HOME;
-        const uri = vscode.window.activeTextEditor?.document.uri;
-        if (!uri || !home) return;
-        const path = uri.path;
-        const wdir = vscode.workspace.getWorkspaceFolder(uri)?.uri.path;
-        let term = vscode.window.activeTerminal;
-        writeFileSync(join(home, ".vscode-path"),
-                      path + "\0" + (wdir ?? ""));
-        if (term) {
-          term.sendText("\x1b[9;9V", false);
-        } else {
-          term = vscode.window.createTerminal({ cwd: dirname(path) });
-        }
-        term.show(false);
-      }),
-    // vscode.commands.registerCommand(
-    //   "elicode.shellEnter",
-    //   () => {
-    //     // vscode.window.showInformationMessage(`FOO`);
-    //     const term = vscode.window.activeTerminal;
-    //     if (!term) return;
-    //     const home = env.HOME;
-    //     const path = vscode.window.activeTextEditor?.document.uri.fsPath;
-    //     if (path && home) {
-    //       writeFileSync(
-    //         join(home, ".preexec"),
-    //         `F='${path.replace(/'/g, "'\"'\"'")}'\n`);
-    //     }
-    //     term.sendText("");
-    //   }),
-  );
+  commands.forEach(c =>
+    context.subscriptions.push(vscode.commands.registerCommand(
+      `elicode.${c.name}`, c)));
+  editorCommands.forEach(c =>
+    context.subscriptions.push(vscode.commands.registerTextEditorCommand(
+      `elicode.${c.name}`, c)));
 };
 
 export const deactivate = () => {};
